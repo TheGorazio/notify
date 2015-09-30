d2a225220935a09cccaee92052522e0ac93c96f9
TheGorazio <thegorazio@gmail.com>
2015-09-30 12:43:33 +0300

    add some php files

---
+++ php/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>#Notify</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		
+		<a href="http://85.30.249.228/"><h1>#Notify</h1></a>
+
+		<ul class="navigate">
+			<li><a href="http://85.30.249.229/">Home</a></li>	
+			<li><a href="signin.php">Sign in</a></li>	
+			<li><a href="login.php">Log in</a></li>	
+			<li><a href="about.php">About</a></li>	
+		</ul>		
+
+	</header>
